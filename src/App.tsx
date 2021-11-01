c357184b3eeb7e0e7c7d5c3e223362666ea1d5a6
Your Name <you@example.com>
2021-11-01 18:50:56 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+interface IProps{
+    
+invoiceData:{
+  
+    invoices:{id:number,name:string,city:string}[];
+  
+}
+logo:string
+}
+const Invoice=(props:IProps)=>{
+  //  const [invoices]=props.invoiceData.invoices;
+    // <h3>{props.invoiceData.invoices[0].name}</h3>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <>
+        <h2>Invoices</h2>
+       <h3>{props.logo}</h3>
+   <table>   
+       <thead>
+           <tr>
+               <th>city</th>
+               <th>name</th>
+               <th>id</th>
+           </tr>
+       </thead>
+      <tbody>
+       
+           
+{
+    props.invoiceData.invoices.map(it=>{
+        return (
+           <tr key={it.id}> 
+          <td>  {it.city}</td><td> {it.name}</td><td>    {it.id}</td>
+          </tr>
+        )
+    })
 }
 
-export default App;
+</tbody>
+</table>
+        </>
+    )
+}
+export const App = () => {
+  const logo="pak"
+    const data={
+       
+     invoices:   [{id:1,name:"asad",city:"lhr"},
+         {id:2,name:"aslam",city:"lkhi"},
+          {id:3,name:"asim",city:"khi"},]
+
+    }
+    return(
+
+        <>
+          <Invoice  invoiceData={data} logo={logo} />
+        </>
+    )
+}
